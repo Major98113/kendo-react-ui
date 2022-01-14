15a3e34a88ab03d98f82b91b234a4cb71e517cbb
Maksim <meison98@bk.ru>
2022-01-14 22:17:24 +0300

    Refactored part of code

--- src/app/main.jsx
+++ src/app/main.jsx
@@ -1,69 +1,43 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Filter, Operators, TextFilter, NumericFilter, BooleanFilter } from '@progress/kendo-react-data-tools';
-import { filterBy } from '@progress/kendo-data-query';
-import { Grid, GridColumn } from '@progress/kendo-react-grid';
-import products from './products.json';
-const initialFilter = {
-  logic: 'and',
-  filters: [{
-    field: 'UnitPrice',
-    operator: 'gt',
-    value: 20
-  }, {
-    field: 'UnitPrice',
-    operator: 'lt',
-    value: 50
-  }, {
-    field: 'Discontinued',
-    operator: 'eq',
-    value: false
-  }, {
-    logic: 'or',
-    filters: [{
-      field: 'ProductName',
-      operator: 'contains',
-      value: 'organic'
-    }, {
-      field: 'ProductName',
-      operator: 'contains',
-      value: 'cranberry'
-    }]
-  }]
+import { Filter, Operators, TextFilter } from '@progress/kendo-react-data-tools';
+
+const  featuresResponse = {
+  data: [
+    { id: 1, name: 'NotificationsMute' },
+    { id: 2, name: 'Share' },
+    { id: 3, name: 'ShareFeed' },
+    { id: 4, name: 'Onboarding' },
+    { id: 5, name: 'ReportErrors' }
+  ], 
+  total: 5
 };
 
 const App = () => {
-  const [filter, setFilter] = React.useState(initialFilter);
-
+  const [features, setFeatures] = React.useState([]);
+  const [filter, setFilter] = React.useState({
+    logic: 'and',
+    filters: []
+  });
   const onFilterChange = event => {
     setFilter(event.filter);
   };
 
-  return <React.Fragment>
-        <Filter value={filter} onChange={onFilterChange} fields={[{
-      name: "ProductName",
-      label: 'Name',
+  React.useEffect( async () => {
+    const featuresFields = featuresResponse.data.map(({name}) => ({
+      name,
+      label: name,
       filter: TextFilter,
-      operators: Operators.text
-    }, {
-      name: "UnitPrice",
-      label: 'Price',
-      filter: NumericFilter,
       operators: Operators.numeric
-    }, {
-      name: "Discontinued",
-      label: 'Discontinued',
-      filter: BooleanFilter,
-      operators: Operators.boolean
-    }]} />
-        <Grid style={{
-      maxHeight: '400px'
-    }} data={filterBy(products, filter)}>
-          <GridColumn field="ProductName" title="Name" width="300px" />
-          <GridColumn field="UnitPrice" title="Price" />
-          <GridColumn field="Discontinued" title="Discontinued" />
-        </Grid>
-      </React.Fragment>;
+    }));
+    setFeatures(featuresFields);
+  }, []);
+
+  return (
+    <React.Fragment>
+      <Filter value={filter} onChange={onFilterChange} fields={features} />
+    </React.Fragment>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector('my-app'));
